69303b6998733a3f72efa43f06d04e17223cf92a
BigDaddyFromGeorgievsk <valerykarapetov@gmail.com>
2024-03-17 18:47:55 +0300

    fix tests4

--- project2/tests/test_1.py
+++ project2/tests/test_1.py
@@ -9,7 +9,7 @@ def reminder_two_numbers(a, b):
     (10, 3, 1),
     (11, 3, 2),
     (12, 4, 0),
-    (21, 3, 7),
+    (21, 3, 0),
     (26, 4, 2),
     ])
 def test_reminder_two_numbers(a, b, expected):
